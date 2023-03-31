1ce9c510b530d9867b6db77dcdb7a0ef6ee01dc3
Maxim Rotar <maxim20003244@gmail.com>
2023-04-01 00:10:36 +0100

    Building About Page of Eazy School Web Application

--- src/main/java/com/eazybyte/springschoolproject/config/WebConfig.java
+++ src/main/java/com/eazybyte/springschoolproject/config/WebConfig.java
@@ -8,5 +8,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
     }
 }
