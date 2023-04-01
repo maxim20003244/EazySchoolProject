ee980893abb63cdb9559ed93b237d50a9d1bca38
Maxim Rotar <maxim20003244@gmail.com>
2023-04-01 17:35:47 +0100

    Create Contact page, and Submit information from contact page

---
+++ src/main/java/com/eazybyte/springschoolproject/controller/ContactController.java
@@ -0,0 +1,31 @@
+package com.eazybyte.springschoolproject.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+public class ContactController {
+  private static Logger log = LoggerFactory.getLogger(ContactController.class);
+    @RequestMapping("/contact")
+    public String displayContactPAge(){
+        return "contact";
+    }
+@RequestMapping(value = "/saveMsg",method = POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+        log.info("Name : " + name);
+        log.info("Mobile Number : " + mobileNum);
+        log.info("Email Address : " + email);
+        log.info("Subject : " + subject);
+        log.info("Message : " + message);
+        return new ModelAndView("redirect:/contact");
+    }
+
+
+}
