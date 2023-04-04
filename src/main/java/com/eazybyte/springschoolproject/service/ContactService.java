7ed9629e90d5fecf2eaa0c4f3ac1ce8b52643ae8
Maxim Rotar <maxim20003244@gmail.com>
2023-04-04 22:12:12 +0100

    Revert "Create Mail service and configure Controller for them"
    This reverts commit d539576f8937b91c830175b332dbd5cbd49f800b.

--- src/main/java/com/eazybyte/springschoolproject/service/ContactService.java
+++ src/main/java/com/eazybyte/springschoolproject/service/ContactService.java
@@ -1,8 +1,10 @@
 package com.eazybyte.springschoolproject.service;
 
 import com.eazybyte.springschoolproject.model.Contact;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +17,4 @@ public class ContactService {
         log.info(contact.toString());
         return isSaved;
     }
-    public String saveMessageString (Contact contact){
-        String save ;
-        save = contact.toString();
-        return save;
-    }
-
-
 }
