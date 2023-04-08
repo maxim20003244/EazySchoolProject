69487ee93339105acca9aa8c9168833a5c9c2fad
Maxim Rotar <maxim20003244@gmail.com>
2023-04-08 13:55:02 +0100

    Exception handing using @ExceptionHandler and @ControllerAdvice

--- src/main/java/com/eazybyte/springschoolproject/controller/DashboardController.java
+++ src/main/java/com/eazybyte/springschoolproject/controller/DashboardController.java
@@ -2,12 +2,9 @@ package com.eazybyte.springschoolproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Slf4j
@@ -17,7 +14,9 @@ public class DashboardController {
     public String displayDashboard(Model model,Authentication authentication){
         model.addAttribute("username",authentication.getName());
         model.addAttribute("roles",authentication.getAuthorities().toString());
-        return "dashboard.html";
+        throw  new RuntimeException("Is exception!");
+        //return "dashboard.html";
+
 
 
     }
