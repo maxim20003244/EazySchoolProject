69bfc8d73fd3fbcd48a553cd2a9f68fb96ee7c4f
Maxim Rotar <maxim20003244@gmail.com>
2023-05-04 22:11:44 +0100

    Writing Custom Queries using @Query annot.

--- src/main/java/com/eazybyte/springschoolproject/repository/ContactRepository.java
+++ src/main/java/com/eazybyte/springschoolproject/repository/ContactRepository.java
@@ -3,11 +3,15 @@ import com.eazybyte.springschoolproject.model.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
     List <Contact> findByStatus (String status);
+
+   // @Query("select c from Contact c where c.status = :status")
+    @Query(value = "select * from contact_msg  where  status = :status", nativeQuery = true)
     Page<Contact> findByStatus(String status , Pageable pageable);
 }
