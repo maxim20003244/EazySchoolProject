e2818b712317ce75cb30cd1124b20c173e3bf2b9
Maxim Rotar <maxim20003244@gmail.com>
2023-05-11 21:39:25 +0100

    Logging Configuration inside SpringBoot and Properties configuration

--- src/main/java/com/eazybyte/springschoolproject/controller/DashboardController.java
+++ src/main/java/com/eazybyte/springschoolproject/controller/DashboardController.java
@@ -5,6 +5,7 @@ import com.eazybyte.springschoolproject.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
     @Autowired
     private PersonRepository personRepository;
+    @Value("${eazyschool.pageSize}")
+    private int defaultPageSize;
+
+    @Value("${eazyschool.contact.successMessages}")
+    private String messages;
     @RequestMapping("/dashboard")
     public String displayDashboard(Model model, Authentication authentication, HttpSession session){
         Person person = personRepository.readByEmail(authentication.getName());
@@ -24,7 +30,18 @@ public class DashboardController {
             model.addAttribute("enrolledClass", person.getEazyClass().getName( ));
         }
         session.setAttribute("LoggedInPerson",person);
+        logMessages();
         return "dashboard.html";
 
+    }
+    private  void logMessages(){
+        log.error("Error messages from Dashboard page");
+        log.warn("Warn messages from Dashboard page");
+        log.info("Info messages from Dashboard page");
+        log.trace("Trace messages from Dashboard page");
+        log.trace("Debug messages from Dashboard page");
+
+        log.error("defaultPageSize value with @Value annotation is : " + defaultPageSize);
+       log.error("successMsg value with @Value annotation is : " + messages);
     }
 }
