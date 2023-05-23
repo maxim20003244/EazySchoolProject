e224513286c3c6684a8424d88dbf3e33f4857c7a
Maxim Rotar <maxim20003244@gmail.com>
2023-05-23 22:36:15 +0100

    Creating beans conditionally based on active profile

--- src/main/java/com/eazybyte/springschoolproject/security/EazyScholNonProdUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/eazybyte/springschoolproject/security/EazyScholNonProdUsernamePwdAuthenticationProvider.java
@@ -3,7 +3,6 @@ package com.eazybyte.springschoolproject.security;
 import com.eazybyte.springschoolproject.model.Person;
 import com.eazybyte.springschoolproject.model.Roles;
 import com.eazybyte.springschoolproject.repository.PersonRepository;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -17,10 +16,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
 @Component
-@Profile("prod")
-public class EazySchollUserNameAuthenticationProvider implements AuthenticationProvider {
+@Profile("!prod")
+public class EazyScholNonProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private PersonRepository personRepository;
     @Autowired
@@ -30,11 +28,11 @@ public class EazySchollUserNameAuthenticationProvider implements AuthenticationP
         String email = authentication.getName();
         String pwd = authentication.getCredentials().toString();
         Person person = personRepository.readByEmail(email);
-        if(null!= person && person.getPersonId()>0 && passwordEncoder.matches(pwd,person.getPwd())){
+        if(null!= person && person.getPersonId()>0){
             return new UsernamePasswordAuthenticationToken(
                     email,
                     null,
-                     getGrantedAuthorities(person.getRoles()));
+                    getGrantedAuthorities(person.getRoles()));
         }
         return null;
     }
